41fc3e11aa2383cfc09ce021bb03048ff78a3008
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-07-16 12:33:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,12 @@ std::string command;
 int main(int argc, char ** argv)
 {
   if(argc > 2){
-    command = "cat index.template.html > ";
-    command += argv[2]; 
-    system(command.c_str());
     TSV tsv(argv[1],argv[2]);
   } 
   else{
     std::string name;
     getline(std::cin, name);
     getline(std::cin, dest);
-    command = "cat index.template.html > ";
-    command += dest; 
-    system(command.c_str());
     TSV tsv(name, dest);
   }
   return 0;
