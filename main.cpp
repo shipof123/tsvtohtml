828fd61c5ee9c36ee9b555cd5f54a6ac34eb40b3
shipof123 <34876781+shipof123@users.noreply.github.com>
2018-10-31 16:50:19 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,13 @@
 
 #include "tsv.h"
 
-std::string dest;
-std::string command;
-
 int main(int argc, char ** argv)
 {
   if(argc > 2){
     TSV tsv(argv[1],argv[2]);
   } 
-  else{
+  else {
+    std::string dest;
     std::string name;
     getline(std::cin, name);
     getline(std::cin, dest);
